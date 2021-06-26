96d7979b4cfe5c7f8f345bcfc25db0fdada58b82
200721968 <pluisgarcia@gmail.com>
2021-06-25 23:22:15 -0600

    porque no funciona con pages

---
+++ Proyecto1/ProyectoIA_SERVER/index.js
@@ -0,0 +1,16 @@
+
+const principal = require("./ejecucion.js");
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || 3000
+
+app.get('/minmax', function (req, res) {
+    console.log("Inicio-Reversi")
+    console.log(req.query)
+    res.send(principal.principal(req.query.estado,req.query.turno));
+});
+
+app.listen(port, () => {
+	console.log("Started in Port: ", port);
+});
